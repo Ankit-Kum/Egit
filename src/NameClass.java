aaaaa74cd067ea425d5f081d933dd129958a60e4
mohit21 <mohit2111695@gmail>
2022-08-03 12:22:28 +0530

    hi lead, This is update for hyperlink as instructed

--- src/NameClass.java
+++ src/NameClass.java
@@ -1,9 +1,25 @@
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 
 public class NameClass {
 
 
 	public void prinName() {
 System.out.println("This is mohit coding here");
+
+HyperlinkListener link= new HyperlinkListener() {
+	
+	@Override
+	public void hyperlinkUpdate(HyperlinkEvent e) {
+		// TODO Auto-generated method stub
+			
+	}
+};
+
+
+
+
+
 	}
 
 }
